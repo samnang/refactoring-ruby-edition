598d9690aa8a38d8190f5d2e52deb5291c895961
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-11 21:17:44 +0700

    [chapter 01 #12] move method Customer#amount_for to Rental#charge

--- chapter_01/after/customer.rb
+++ chapter_01/after/customer.rb
@@ -14,7 +14,7 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      this_amount = amount_for(element)
+      this_amount = element.charge
 
       # add frequent renter points
       frequent_renter_points += 1
@@ -30,19 +30,5 @@ class Customer
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
-  end
-
-  def amount_for(rental)
-    result = 0
-    case rental.movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-    when Movie::NEW_RELEASE
-      result += rental.days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-    end
   end
 end
