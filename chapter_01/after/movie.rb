a129d9b8b893b38b30155febb3c88ab46ad1e330
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-12 00:01:18 +0700

    [chapter 01 #36] Replace Type Code with State/Strategy

--- chapter_01/after/movie.rb
+++ chapter_01/after/movie.rb
@@ -1,30 +1,52 @@
 class Movie
-  REGULAR = 0
-  NEW_RELEASE = 1
-  CHILDRENS = 2
-
   attr_reader :title
-  attr_accessor :price_code
+  attr_writer :price
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def initialize(title, price)
+    @title, @price = title, price
   end
 
   def charge(days_rented)
-    result = 0
-    case price_code
-    when Movie::REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when Movie::NEW_RELEASE
-      result += days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
-    (price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+    @price.frequent_renter_points(days_rented)
+  end
+end
+
+module DefaultPrice
+  def frequent_renter_points(days_rented)
+    1
+  end
+end
+
+class RegularPrice
+  include DefaultPrice
+
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+    result
+  end
+end
+
+class NewReleasePrice
+  def charge(days_rented)
+    days_rented * 3
+  end
+
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
+end
+
+class ChildrensPrice
+  include DefaultPrice
+
+  def charge(days_rented)
+    result = 1.5
+    result += (days_rented - 3) * 1.5 if days_rented > 3
+    result
   end
 end
