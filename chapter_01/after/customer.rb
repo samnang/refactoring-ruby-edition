0a124ca70652ac7165dfba75350b3e3671b224aa
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-11 21:38:00 +0700

    [chapter 01 #22] removing temps with query, so we can reuse calculations in Customer#html_statement

--- chapter_01/after/customer.rb
+++ chapter_01/after/customer.rb
@@ -11,17 +11,38 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      frequent_renter_points += element.frequent_renter_points
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
-      total_amount += element.charge
     end
     # add footer lines
-    result += "Amount owed is #{total_amount}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "Amount owed is #{total_charge}\n"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
+  end
+
+  def html_statement
+    result = "<h1>Rentals for <em>#{@name}</em></h1><p>\n"
+    @rentals.each do |element|
+      # show figures for this rental
+      result += "\t" + element.movie.title + ": " + element.charge.to_s + "<br>\n"
+    end
+    # add footer lines
+    result += "<p>You owe <em>#{total_charge}</em><p>\n"
+    result += "On this rental you earned " +
+      "<em>#{total_frequent_renter_points}</em> " +
+      "frequent renter points<p>"
+    result
+  end
+
+  private
+
+  def total_charge
+    @rentals.inject(0) {|sum, element| sum += element.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) {|sum, element| sum += element.frequent_renter_points }
   end
 end
