1451a6d5852ba790e30230f977538b53f9e890d1
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-11 22:00:43 +0700

    [chapter 01 #33] move methods because the proposed changes all about adding types, so calculation should be in movie

--- chapter_01/after/movie.rb
+++ chapter_01/after/movie.rb
@@ -9,4 +9,22 @@ class Movie
   def initialize(title, price_code)
     @title, @price_code = title, price_code
   end
+
+  def charge(days_rented)
+    result = 0
+    case price_code
+    when Movie::REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when Movie::NEW_RELEASE
+      result += days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+  end
+
+  def frequent_renter_points(days_rented)
+    (price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+  end
 end
