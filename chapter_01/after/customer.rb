f63a6829133881403a0287e845b3acee8ff6854f
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-11 21:26:47 +0700

    [chapter 01 #18] extracting frequent renter points

--- chapter_01/after/customer.rb
+++ chapter_01/after/customer.rb
@@ -14,12 +14,7 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      # add frequent renter points
-      frequent_renter_points += 1
-      # add bonus for a two day new release rental
-      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
-        frequent_renter_points += 1
-      end
+      frequent_renter_points += element.frequent_renter_points
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
       total_amount += element.charge
