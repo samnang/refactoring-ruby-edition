854af7779e7a147e91246a9d0a35752c183c40fd
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-23 01:59:26 +0700

    [chapter 06 #116] Replace Temp with Chain: create a method to create itself and do the action

--- chapter_06/replace_temp_with_chain/after/select.rb
+++ chapter_06/replace_temp_with_chain/after/select.rb
@@ -1,4 +1,10 @@
 class Select
+  def self.with_option(option)
+    select = self.new
+    select.options << option
+    select
+  end
+
   def options
     @options ||= []
   end
--- chapter_06/replace_temp_with_chain/after/spec/select_spec.rb
+++ chapter_06/replace_temp_with_chain/after/spec/select_spec.rb
@@ -2,12 +2,11 @@ require_relative '../select'
 
 describe Select do
   it "be able to add options" do
-    select = Select.new
-    expect(select.options).to be_empty
+    select = Select.with_option(0)
+    expect(select.options).to eql([0])
 
     select.add_option(1)
     select.add_option(2)
-    expect(select.options).to eql([1, 2])
+    expect(select.options).to eql([0, 1, 2])
   end
 end
-
