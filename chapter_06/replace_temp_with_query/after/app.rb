aacae4855724e2db2338225c2becaf918897562a
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-23 01:25:35 +0700

    [chapter 06 #112] Replace Temp with Query: replace references, then remove temp

--- chapter_06/replace_temp_with_query/after/app.rb
+++ chapter_06/replace_temp_with_query/after/app.rb
@@ -5,8 +5,7 @@ class App
   end
 
   def price
-    a_base_price = base_price
-    if a_base_price > 1000
+    if base_price > 1000
       discount_factor = 0.95
     else
       discount_factor = 0.98
