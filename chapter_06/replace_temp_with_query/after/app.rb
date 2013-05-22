196963369ceb6b8587e98cae838aa88a778a5a58
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-23 01:27:33 +0700

    [chapter 06 #112] Replace Temp with Query: refactored example

--- chapter_06/replace_temp_with_query/after/app.rb
+++ chapter_06/replace_temp_with_query/after/app.rb
@@ -5,16 +5,16 @@ class App
   end
 
   def price
-    if base_price > 1000
-      discount_factor = 0.95
-    else
-      discount_factor = 0.98
-    end
-    
     base_price * discount_factor
   end
 
+  private
+
   def base_price
     @quantity * @item_price
+  end
+
+  def discount_factor
+    base_price > 1000 ? 0.95 : 0.98
   end
 end
