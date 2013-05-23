bb44000073ad82d9168699d983600a617a5eecdf
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-23 21:02:09 +0700

    [chapter 06 #126] Remove Assignments to Parameters: refactored example

--- chapter_06/remove_assignments_to_parameters/after/app.rb
+++ chapter_06/remove_assignments_to_parameters/after/app.rb
@@ -1,8 +1,9 @@
 class App
   def discount(input_value, quantity, year_to_date)
-    input_value -= 2 if input_value > 50
-    input_value -= 1 if quantity > 100
-    input_value -= 4 if year_to_date > 10000
-    input_value
+    result = input_value
+    result -= 2 if input_value > 50
+    result -= 1 if quantity > 100
+    result -= 4 if year_to_date > 10000
+    result
   end
 end
