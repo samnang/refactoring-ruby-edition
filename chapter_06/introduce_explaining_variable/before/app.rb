6f9cd5a7205d5619d28c015543151580dfc377dd
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-23 12:33:47 +0700

    [chapter 06 #119] Introduce Explaining Variable: refactored example with extracting methods

--- chapter_06/introduce_explaining_variable/before/app.rb
+++ chapter_06/introduce_explaining_variable/before/app.rb
@@ -5,10 +5,20 @@ class App
   end
 
   def price
-    base_price = @quantity * @item_price
-    quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
-    shipping = [base_price * 0.1, 100.0].min
-    return base_price - quantity_discount + shipping
+    base_price - quantity_discount + shipping
+  end
+
+  private
+
+  def base_price
+    @quantity * @item_price
+  end
+
+  def quantity_discount
+    [0, @quantity - 500].max * @item_price * 0.05
+  end
+
+  def shipping
+    [base_price * 0.1, 100.0].min
   end
 end
-
