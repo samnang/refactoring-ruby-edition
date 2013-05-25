0126e463ca1430b6936fe4b7ee24719f3c0d07cf
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-25 15:49:32 +0700

    [chapter 06 #141] Introduce Class Annotation: delete original method by calling class method

--- chapter_06/introduce_class_annotation/after/search_criteria.rb
+++ chapter_06/introduce_class_annotation/after/search_criteria.rb
@@ -1,10 +1,4 @@
 class SearchCriteria
-  def initialize(hash)
-    @author_id = hash[:author_id]
-    @publisher_id = hash[:publisher_id]
-    @isbn = hash[:isbn]
-  end
-
   def self.hash_initializer(*attribute_names)
     define_method(:initialize) do |*args|
       data = args.first || {}
@@ -12,7 +6,9 @@ class SearchCriteria
         instance_variable_set "@#{attribute_name}", data[attribute_name]
       end
     end
+
+    attr_reader *attribute_names
   end
 
-  attr_reader :author_id, :publisher_id, :isbn
+  hash_initializer :author_id, :publisher_id, :isbn
 end
