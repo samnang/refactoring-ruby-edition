72d5c33df246261bb817574d6f54b7bac84731df
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-25 15:46:40 +0700

    [chapter 06 #141] Introduce Class Annotation: Convert the original method to a class method

--- chapter_06/introduce_class_annotation/after/search_criteria.rb
+++ chapter_06/introduce_class_annotation/after/search_criteria.rb
@@ -5,5 +5,14 @@ class SearchCriteria
     @isbn = hash[:isbn]
   end
 
+  def self.hash_initializer(*attribute_names)
+    define_method(:initialize) do |*args|
+      data = args.first || {}
+      attribute_names.each do |attribute_name|
+        instance_variable_set "@#{attribute_name}", data[attribute_name]
+      end
+    end
+  end
+
   attr_reader :author_id, :publisher_id, :isbn
 end
