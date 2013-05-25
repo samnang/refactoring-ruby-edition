d0d1e4fca9e74d18ea8d1ff527ee5d53f5bc581c
Samnang Chhun <samnang.chhun@gmail.com>
2013-05-25 14:57:45 +0700

    [chapter 06 #132] Substitute Algorithm: refactored example

--- chapter_06/substitube_algorithm/before/app.rb
+++ chapter_06/substitube_algorithm/before/app.rb
@@ -1,18 +1,7 @@
 class App
   def found_friends(people)
-    friends = []
-    people.each do |person|
-      if(person == "Don")
-        friends << "Don"
-      end
-      if(person == "John")
-        friends << "John"
-      end
-      if(person == "Kent")
-        friends << "Kent"
-      end
+    people.select do |person|
+      %(Don John Kent).include?(person)
     end
-
-    return friends
   end
 end
