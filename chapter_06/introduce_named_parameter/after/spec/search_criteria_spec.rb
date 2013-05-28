5fbe4108682750456cdab486806607004460eef9
samnang chhun <samnang.chhun@gmail.com>
2013-05-28 21:25:16 +0700

    [chapter 06 #144] Introduce Named Parameter: Replace the parameters in the calling code with name/value pairs

--- chapter_06/introduce_named_parameter/after/spec/search_criteria_spec.rb
+++ chapter_06/introduce_named_parameter/after/spec/search_criteria_spec.rb
@@ -2,7 +2,7 @@ require_relative '../search_criteria'
 
 describe "SearchCriteria" do
   it "intializes fields via hash" do
-    criteria = SearchCriteria.new(1, 1, "123")
+    criteria = SearchCriteria.new(author_id: 1, publisher_id: 1, isbn: "123")
 
     expect(criteria.author_id).to eql(1)
     expect(criteria.publisher_id).to eql(1)
