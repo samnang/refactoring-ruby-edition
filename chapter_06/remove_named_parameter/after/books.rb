f8423623ccb8c3e3b92e841aee16e79b6e949442
samnang chhun <samnang.chhun@gmail.com>
2013-05-29 01:52:16 +0700

    [chapter 06 #149] Remove Named Parameter: add example

---
+++ chapter_06/remove_named_parameter/after/books.rb
@@ -0,0 +1,24 @@
+class Books
+  def self.find(hash={})
+    hash[:joins] ||= []
+    hash[:conditions] ||= ""
+
+    sql = ["SELECT * FROM books"]
+    hash[:joins].each do |join_table|
+      sql << "LEFT OUTER JOIN #{join_table}"
+      sql << "ON books.#{join_table.to_s.chop}_id = #{join_table}.id"
+    end
+
+    sql << "WHERE #{hash[:conditions]}" unless hash[:conditions].empty?
+    sql << "LIMIT 1" if hash[:selector] == :first
+
+    connection.find(sql.join(" "))
+  end
+
+  class << self
+    attr_writer :connection
+    def connection
+      @connection ||= DBConnection.new
+    end
+  end
+end
---
+++ chapter_06/remove_named_parameter/before/spec/books_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../books'
+
+describe Books do
+  it "finds books" do
+    connection = double("Connection")
+    Books.connection = connection
+    sql = "SELECT * FROM books LEFT OUTER JOIN authors ON books.author_id = authors.id WHERE authors.name = 'Jenny James'"
+
+    connection.should_receive(:find).with(sql)
+
+    Books.find(:selector => :all,
+               :conditions => "authors.name = 'Jenny James'",
+               :joins => [:authors])
+
+  end
+end
