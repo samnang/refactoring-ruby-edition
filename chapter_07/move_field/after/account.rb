634faee6c66524175ce33e0e442b84cd8edd5559
samnang chhun <samnang.chhun@gmail.com>
2013-06-01 12:39:17 +0700

    [chapter 07 #174] Move Field: using self-encapsulation if there are many methods referencing the field

--- chapter_07/move_field/after/account.rb
+++ chapter_07/move_field/after/account.rb
@@ -4,6 +4,10 @@ class Account
   end
 
   def interest_for_amount_days(amount, days)
-    @account_type.interest_rate * amount * days / 365
+    interest_rate * amount * days / 365
+  end
+
+  def interest_rate
+    @account_type.interest_rate
   end
 end
