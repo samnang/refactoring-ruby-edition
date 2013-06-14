5b6d632fdf2b442f49f3a7f35215d06ef084ba35
samnang chhun <samnang.chhun@gmail.com>
2013-06-14 16:20:16 +0700

    [chapter 08 #193] Replace Data Value with Object: change method to reference to use customer class

--- chapter_08/replace_data_value_with_object/after/order.rb
+++ chapter_08/replace_data_value_with_object/after/order.rb
@@ -1,8 +1,14 @@
 class Order
-  attr_accessor :customer
-
   def initialize(customer)
-    @customer = customer
+    @customer = Customer.new(customer)
+  end
+  
+  def customer
+    @customer.name
+  end
+
+  def customer=(value)
+    @customer = Customer.new(value)
   end
 
   def self.number_of_orders_for(orders, customer)
