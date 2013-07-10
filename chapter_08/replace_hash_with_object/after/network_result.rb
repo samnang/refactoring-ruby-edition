f27c2da256b0341a552bb98db5e78098406a0dbf
samnang chhun <samnang.chhun@gmail.com>
2013-07-10 15:50:16 +0700

    [chapter 08 #209] Replace Hash with Object: refactored example

--- chapter_08/replace_hash_with_object/after/network_result.rb
+++ chapter_08/replace_hash_with_object/after/network_result.rb
@@ -1,21 +1,17 @@
 class NetworkResult
+  attr_reader :old_networks, :nodes
+  attr_accessor :name
+
   def initialize
     @nodes, @old_networks = [], []
   end
 
-  def [](attribute)
-    instance_variable_get "@#{attribute}"
-  end
-  def []=(attribute, value)
-    instance_variable_set "@#{attribute}", value
+  def name
+    @old_networks.collect {|network| network.name }.join(" - ")
   end
 end
 
 new_network = NetworkResult.new
 
-new_network[:old_networks] << node.network
-new_network[:nodes] << node
-
-new_network[:name] = new_network[:old_networks].collect do |network|
-  network.name
-end.join(" - ")
+new_network.old_networks << node.network
+new_network.nodes << node
